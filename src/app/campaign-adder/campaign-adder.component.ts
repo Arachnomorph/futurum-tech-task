a384bbd2c1db86043cc24e46967238fa16569792
Wojciech Pająk <arachnomorph@gmail.com>
2025-02-01 15:39:17 +0100

    add form values handling

--- src/app/campaign-adder/campaign-adder.component.ts
+++ src/app/campaign-adder/campaign-adder.component.ts
@@ -8,11 +8,19 @@ import { ReactiveFormsModule, FormControl, FormGroup } from '@angular/forms';
   styleUrl: './campaign-adder.component.scss',
 })
 export class CampaignAdderComponent {
+  towns = ['', 'London', 'Manchester', 'Liverpool', 'Birmingham']; //TODO: Get this from a service
+
   campaignForm = new FormGroup({
     campaignName: new FormControl(''),
+    campaignKeywords: new FormControl(''),
+    campaignBid: new FormControl(0),
+    campaignFund: new FormControl(0),
+    campaignStatus: new FormControl(''),
+    campaignTown: new FormControl(this.towns[0]),
+    campaignRadius: new FormControl(''),
   });
 
   addCampaign() {
-    console.log(this.campaignForm.value.campaignName);
+    console.log(this.campaignForm.value);
   }
 }
